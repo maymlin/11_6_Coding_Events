39d9c7662bb57f6b383d8ed6f6b2d1583773c441
May Lin <40640630+maymlin@users.noreply.github.com>
2021-09-13 16:46:05 -0500

    Complete 11.6 Thymeleaf Forms

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,71 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    // 11.6.7. Add a Form Handler Method - Text
+    private static List<String> events = new ArrayList<>();
+
+    /**
+    // https://www.youtube.com/watch?v=hmgxMOf51JU
+    // 11.6.2 Coding Events Setup - Video
+    // replaced in "11.6.7. Add a Form Handler Method - Text" below
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        List<String> events = new ArrayList<>(
+                List.of("Coding With Pride",
+                        "Strange Loop",
+                        "Apple WWDC",
+                        "SpringOne Platform")
+        );
+        model.addAttribute("events", events);
+
+        return "events/index";
+    }
+    */
+
+    // https://www.youtube.com/watch?v=LnpJcq33uoM
+    // 11.6.7. Add a Form Handler Method - Text
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    // https://www.youtube.com/watch?v=lgT962si4eQ
+    // 11.6.5. Create and Render a Form - Text
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    // https://www.youtube.com/watch?v=LnpJcq33uoM
+    // 11.6.7. Add a Form Handler Method - Text
+    // lives at /events/create  -> Okay because renderCreateEventForm handles GET event
+    // and this controller handles POST event
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:"; // return a 300 redirect response
+    }
+
+//    @PostMapping("create")
+//    public String processForm(@RequestParam String event, Model model) {
+//
+//        return "events/index";
+//    }
+}
