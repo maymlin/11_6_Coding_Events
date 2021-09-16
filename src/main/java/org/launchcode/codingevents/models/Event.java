e7a403edf504740a6df9327a0370c33ae54126b1
May Lin <40640630+maymlin@users.noreply.github.com>
2021-09-16 15:01:57 -0500

    Complete 12.3.2 Add a Unique Id - Text

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,27 @@
 package org.launchcode.codingevents.models;
 
 public class Event {
+    private int id;
+    private static int nextId = 1;
     private String name;
     private String eventDescription;
 
     public Event(String eventName, String eventDescription) {
         this.name = eventName;
         this.eventDescription = eventDescription;
+        this.id = this.nextId;
+        this.nextId++;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setEventName(String eventName) {
+    public void setName(String eventName) {
         this.name = eventName;
     }
 
