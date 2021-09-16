947e163e90ca1133b2d881089f20a38631e3fd97
May Lin <40640630+maymlin@users.noreply.github.com>
2021-09-15 22:54:00 -0500

    Start 12.2 Create a Model

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+
 
     // 11.6.7. Add a Form Handler Method - Text
     private static List<String> events = new ArrayList<>();
@@ -53,6 +55,18 @@ public class EventController {
         return "events/create";
     }
 
+
+    // https://www.youtube.com/watch?v=5dtyojtADbk
+    // 12.2. Create a Model
+    @PostMapping("create")
+    public String processCreateEventForm(@RequestParam String eventName,
+                                         @RequestParam String eventDescription) {
+        events.add(new Event(eventName, eventDescription));
+        return "redirect:";
+    }
+
+    /*
+    // replaced by processCreateEventForm() in 12.2 Create a Model to accommodate models
     // https://www.youtube.com/watch?v=LnpJcq33uoM
     // 11.6.7. Add a Form Handler Method - Text
     // lives at /events/create  -> Okay because renderCreateEventForm handles GET event
@@ -62,6 +76,7 @@ public class EventController {
         events.add(eventName);
         return "redirect:"; // return a 300 redirect response
     }
+     */
 
 //    @PostMapping("create")
 //    public String processForm(@RequestParam String event, Model model) {
---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,9 @@
+package org.launchcode.codingevents.models;
+
+public class Event {
+    private String eventName;
+    private String eventDescription;
+    public Event(String eventName, String eventDescription) {
+
+    }
+}
