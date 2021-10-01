88a9b243e0797cb768bffcfbee50393385603ff0
May Lin <40640630+maymlin@users.noreply.github.com>
2021-09-30 19:32:09 -0500

    Complete 19.5.3 Registering the Filter With Spring - Add WebApplicationConfig class

---
+++ src/main/java/org/launchcode/codingevents/WebApplicationConfig.java
@@ -0,0 +1,23 @@
+package org.launchcode.codingevents;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 19.5.3 Registering the Filter with Spring
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+    // Create spring-managed object to allow the app to access our filter
+    @Bean
+    public AuthenticationFilter authenticationFilter() {
+        return new AuthenticationFilter();
+    }
+
+    // Register the filter with the Spring container
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationFilter());
+    }
+}
